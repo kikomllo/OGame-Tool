63755cdcde8d6903c6f6254720ab3280f5b1f2bf
kikomllo <a84085@ualg.pt>
2026-04-17 19:48:32 +0100

    Fixed version number

--- ogameTool.js
+++ ogameTool.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OGame Tool
 // @namespace    http://tampermonkey.net/
-// @version      1.26
+// @version      1.27
 // @description  My First Script, hope you enjoy!
 // @author       You
 // @match        *://*.ogame.gameforge.com/*
